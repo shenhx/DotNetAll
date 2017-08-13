d3b7a453a1c439145f36ecb4323b0f4980227d3d
shenhx <hongxiao_shen@kingdee.com>
2017-08-13 16:55:40 +0800

    添加wcf项目 1.增加swagger测试

---
+++ WcfServiceDemo/WcfServiceDemo/Global.asax.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Activation;
+using System.Web;
+using System.Web.Routing;
+using SwaggerWcf.Models;
+using SwaggerWcf;
+
+namespace WcfServiceDemo
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //添加路由地址前缀，如有多个接口，需要添加多个
+            RouteTable.Routes.Add(new ServiceRoute("v1/rest", new WebServiceHostFactory(), typeof(Service1)));
+            //swagger测试地址：http://localhost:1370/api-docs，端口号可能会变
+            RouteTable.Routes.Add(new ServiceRoute("api-docs", new WebServiceHostFactory(), typeof(SwaggerWcfEndpoint)));
+
+            var info = new Info
+            {
+                Description = "Sample Service to test SwaggerWCF",
+                Version = "0.0.1"
+                // etc
+            };
+
+            var security = new SecurityDefinitions
+            {
+                {
+                    "api-gateway", new SecurityAuthorization
+                    {
+                        Type = "oauth2",
+                        Name = "api-gateway",
+                        Description = "Forces authentication with credentials via an api gateway",
+                        Flow = "implicit",
+                        Scopes = new Dictionary<string, string>
+                        {
+                            { "fu", "use fu scope"},
+                            { "bar", "use bar scope"},
+                        },
+                        AuthorizationUrl = "http://yourapi.net/oauth/token"
+                    }
+                }
+            };
+            //配置Swagger
+            SwaggerWcfEndpoint.Configure(info, security);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ WcfServiceDemo/WcfServiceDemo/Service1.svc.cs
@@ -0,0 +1,32 @@
+﻿using SwaggerWcf.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceDemo
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Service1”。
+    // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 Service1.svc 或 Service1.svc.cs，然后开始调试。
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [SwaggerWcf("/v1/rest")]
+    public class Service1 : IService1
+    {
+        /// <summary>
+        /// 返回Hello World
+        /// </summary>
+        /// <returns></returns>
+        [SwaggerWcfTag("Test")]
+        [SwaggerWcfResponse(HttpStatusCode.OK, "hello World!")]
+        [SwaggerWcfResponse(HttpStatusCode.NoContent, "error", true)]
+        public string SayHelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
