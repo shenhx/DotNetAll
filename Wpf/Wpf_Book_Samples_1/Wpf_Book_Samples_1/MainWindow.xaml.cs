159d0cf605bb718ac6dfacde7992484a27b906ac
shenhx <623724489@qq.com>
2018-01-23 01:03:48 +0800

    明天再合并冲突

--- Wpf/Wpf_Book_Samples_1/Wpf_Book_Samples_1/MainWindow.xaml.cs
+++ Wpf/Wpf_Book_Samples_1/Wpf_Book_Samples_1/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Sample.DependencyObjectTest;
-using Sample.RouteEventTest;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,66 +22,8 @@ namespace Wpf_Book_Samples_1
         public MainWindow()
         {
             InitializeComponent();
-            this.SayHello += MainWindow_SayHello;
-            ////引发路由事件
-            RoutedEventArgs e = new RoutedEventArgs(MyRouteEvent.SayHelloEvent, this);
-            base.RaiseEvent(e);
         }
-
-        private void MainWindow_SayHello(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("hello");
-            //throw new Exception("qqq");
-        }
-
-        //一般在自定义控件中使用，在这里定义会出现线程占用问题。
-        ////第二步：注册依赖项属性
-        //static MainWindow()
-        //{
-        //    //共享的依赖属性
-        //    MainWindow.IsDefaultProperty = MyDependencyObject.IsDefaultProperty.AddOwner(typeof(MainWindow));
-
-        //    //共享路由事件
-        //    MainWindow.SayHelloEvent = MyRouteEvent.SayHelloEvent.AddOwner(typeof(MainWindow));
-        //}
-
-        #region 依赖属性
-        //第一步：定义依赖属性(属性名称+Property)
-        public static readonly DependencyProperty IsDefaultProperty;
-
         
-
-        //第三步：添加属性包装器
-        public bool IsDefault
-        {
-            get { return (bool)GetValue(IsDefaultProperty); }
-            set { SetValue(IsDefaultProperty, value); }
-        }
-        #endregion
-
-        #region 共享路由事件
-        //第一步：定义路由事件
-        public static readonly RoutedEvent SayHelloEvent;
-        
-        //按传统方式封装事件
-        public event RoutedEventHandler SayHello
-        {
-            add { base.AddHandler(MyRouteEvent.SayHelloEvent, value); }
-            remove { base.RemoveHandler(MyRouteEvent.SayHelloEvent, value); }
-        }
-        #endregion
-
-        /// <summary>
-        /// todo:需要改造成命令模式
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Window_Binding binding = new Window_Binding();
-            binding.ShowDialog();
-        }
-
         private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             if (e.Parameter != null && !string.IsNullOrEmpty(e.Parameter.ToString()))
@@ -98,14 +38,22 @@ namespace Wpf_Book_Samples_1
             {
                 case "Binding":
                     {
-                        Window_Binding binding = new Window_Binding();
-                        binding.ShowDialog();
                         break;
                     }
                 case "Command":
                     {
-                        Window_Command command= new Window_Command();
-                        command.ShowDialog();
+                        break;
+                    }
+                case "DeepInAnimation":
+                    {
+                        Window_DeepInAnimation deepInAnimation = new Window_DeepInAnimation();
+                        deepInAnimation.ShowDialog();
+                        break;
+                    }
+                case "ControlTemplate":
+                    {
+                        Window_ControlTemplate _ControlTemplate = new Window_ControlTemplate();
+                        _ControlTemplate.ShowDialog();
                         break;
                     }
                 default:
