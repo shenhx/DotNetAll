e1c2888fde58f5855c7b504712ac58a3cc469f83
shenhx <623724489@qq.com>
2018-01-24 23:37:00 +0800

    反射知识

---
+++ MyCSharpLib/MyCSharpLib/Reflecting/ReflectingTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ReflectingTest : ReflectingTestBase, IReflectingTest
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("hello world!");
+            base.SayHello();
+        }
+    }
+}
---
+++ MyCSharpLib/MyCSharpLib/Reflecting/ReflectingTestBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public abstract class ReflectingTestBase : IReflectingTest
+    {
+        public virtual void SayHello()
+        {
+        }
+    }
+}
