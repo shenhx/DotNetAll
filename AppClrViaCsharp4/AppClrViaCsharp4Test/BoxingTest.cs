6c985428b8695cc736cfea110cc27c591bbc838d
shenhx <623724489@qq.com>
2018-05-24 01:06:55 +0800

    提交代码

---
+++ AppClrViaCsharp4/AppClrViaCsharp4Test/BoxingTest.cs
@@ -0,0 +1,30 @@
+﻿using AppClrViaCsharp4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppClrViaCsharp4Test
+{
+    [TestClass]
+    public class BoxingTest
+    {
+        [TestMethod]
+        public void Boxing_Test_1()
+        {
+            Point p = new Point(1,1);
+            Console.WriteLine(p);//1,1
+
+            p.Change(2, 2);
+            Console.WriteLine(p);//2,2
+
+            object o = p;
+            Console.Write(o);//2,2
+
+            ((Point)o).Change(3, 3);
+            Console.WriteLine(o);//2,2
+        }
+    }
+}
---
+++ AppClrViaCsharp4/AppClrViaCsharp4Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppClrViaCsharp4Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
