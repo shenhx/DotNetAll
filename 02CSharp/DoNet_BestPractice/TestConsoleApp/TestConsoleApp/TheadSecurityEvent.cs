0a616bad762fb13e8423680ed493756b4b29a96c
shenhx <623724489@qq.com>
2018-08-29 00:10:06 +0800

    提交最佳实践

---
+++ 02CSharp/DoNet_BestPractice/TestConsoleApp/TestConsoleApp/TheadSecurityEvent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TestConsoleApp
+{
+    public class ResponseArgs : EventArgs
+    {
+        public string Message { get; set; }
+    }
+
+    public class TheadSecurityEvent : ITest
+    {
+        public event EventHandler<ResponseArgs> SayHelloEvent;
+
+#if DotNet_45
+        public void Request(string msg) => Volatile.Read(ref SayHello)?.Invoke(this, new ResponseArgs { Message = msg });
+#else
+        public void Request(string msg)
+        {
+            EventHandler<ResponseArgs> tmpEvent = SayHelloEvent;
+            if(tmpEvent != null)
+            {
+                tmpEvent(this, new ResponseArgs { Message = "hello,"+msg });
+            }
+        }
+#endif
+
+
+        /// <summary>
+        /// 测试
+        /// </summary>
+        public void Test()
+        {
+            this.SayHelloEvent += TheadSecurityEvent_SayHelloEvent;
+            this.Request("world");
+        }
+
+        private static void TheadSecurityEvent_SayHelloEvent(object sender, ResponseArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
